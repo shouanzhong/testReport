4bfd5c6054d8dfb91150e613cf4c69cc5136711e
shouanzhong <34148008+shouanzhong@users.noreply.github.com>
2023-08-19 20:21:35 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+function getData(){
+	return {
+    "testPass": 1,
+    "testResult": [
+        {
+            "className": "com.test.testcase.TestDemo1222222",
+            "methodName": "testDemo",
+            "description": "测试DEMO",
+            "spendTime": "11ms",
+            "status": "成功",
+            "log": [
+                "this is demo!"
+            ]
+        }
+    ],
+    "testName": "20171109132744897",
+    "testAll": 1,
+    "testFail": 0,
+    "beginTime": "2017-11-09 13:27:44.917",
+    "totalTime": "11ms",
+    "testSkip": 0
+}
+}
